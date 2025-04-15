0106fc0387f130b08ce4adcb59cbb249d0080b34
raves21 <jrRanara@mcm.edu.ph>
2025-04-16 05:23:04 +0800

    x

--- api/index.ts
+++ api/index.ts
@@ -1,24 +1,2 @@
-import express, { Request, Response } from "express";
-import m3u8 from "./routes/m3u8-proxy";
-import TsProxy from "./routes/ts-proxy";
-
-const app = express();
-const PORT = 3000;
-
-// Middleware to parse JSON
-app.use(express.json());
-
-// Basic route
-app.get("/", (req: Request, res: Response) => {
-  res.send("BRUH");
-  return;
-});
-app.get("/m3u8-proxy.m3u8", m3u8);
-app.get("/ts-proxy.ts", TsProxy);
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
-
-module.exports = app;
+import app from "../src";
+export default app;
